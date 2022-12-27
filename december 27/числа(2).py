ce0f351b2df3e57d798bf8e2c9777dbcfdf65a92
0GREE <120639948+0GREE@users.noreply.github.com>
2022-12-27 17:53:31 +0800

    Add files via upload

---
+++ december 27/числа(2).py
@@ -0,0 +1,17 @@
+x=int(input("введите число "))
+y=int(input("введите число "))
+z=int(input("введите число "))
+
+if x==y==z:
+    print("три числа равны")
+elif x==y:
+    print("2 числа равны")
+elif x==z:
+    print("2 числа равны")
+elif z==y:
+    print(" числа равны")
+elif x!=y!=z
+    print("ни одно число не равно")
+    
+
+    
