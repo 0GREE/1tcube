2ea4c0bec22e8cc93472b8785aaff3da248af702
0GREE <120639948+0GREE@users.noreply.github.com>
2022-12-27 17:45:13 +0800

    Add files via upload

---
+++ december 27/числа(1).py
@@ -0,0 +1,13 @@
+x=int(input("введите число "))
+y=int(input("введите число "))
+z=int(input("введите число "))
+
+if x==y and x==z and z==y:
+    print("три числа равны")
+elif x==y or x==z or z==y:
+    print("2 числа равны")
+elif x!=y and x!=z and y!=z:
+    print("ни одно число не равно")
+    
+
+    
