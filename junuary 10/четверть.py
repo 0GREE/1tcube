b742e8b036df39788f6f662c26c01c5087ee6111
0GREE <120639948+0GREE@users.noreply.github.com>
2023-01-10 17:11:21 +0800

    четверти

---
+++ junuary 10/четверть.py
@@ -0,0 +1,13 @@
+a=int(input("введите координату x"))
+b=int(input("введите координату y"))
+
+if a>0:
+    if b>0:
+        print("1")
+    else:
+        print("4")\
+else:
+        if b>0:
+            print("2")
+        else:
+            print("3")
