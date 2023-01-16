3103ca56042e17253832a51c2e0dd7fb3bdaa634
0GREE <120639948+0GREE@users.noreply.github.com>
2023-01-16 15:36:07 +0800

    Add files via upload

---
+++ DZ 13.01.22/ферзь.py
@@ -0,0 +1,8 @@
+x1 = int(input("введите x1"))
+y1 = int(input("введите y1"))
+x2 = int(input("введите x2"))
+y2 = int(input("введите y2"))
+if x1 - x2 == y1 - y2 or x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
---
+++ DZ 13.01.22/ход коня.py
@@ -0,0 +1,9 @@
+x1=int(input("X1=?"))
+y1=int(input("Y1=?"))
+x2=int(input("X2=?"))
+y2=int(input("Y2=?"))
+
+if x1-x2==2 and y1-y2==1 or y1-y2==2 and absx1-x2==1:
+    print('YES')
+else:
+    print('NO')
