59e68daa110185b9cb43f10b677dd3389f705adc
0GREE <120639948+0GREE@users.noreply.github.com>
2023-04-13 18:21:45 +0800

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-from plyer import *
-from psutil import *
-
-cputemp=cpu_percent(interval=1)
-if cputemp>21:
-    notification.notify(title='Внимание!',message='Cлишком большая нагрузка на ЦП!',timeout = (15))
-print(cputemp)
-    
-
